469385fe0fd1a59a95a4cd8022899279db45bfec
finking <finking@list.ru>
2024-12-20 16:01:07 +0300

    Added custom filter in admin.py for publications. Added field Accepted in list_display

--- stat_univer/main/admin.py
+++ stat_univer/main/admin.py
@@ -1,5 +1,40 @@
 from django.contrib import admin
+from django.db.models import Count, Q
+from django.db.models.functions import Lower
+
 from .models import Institute, Departure, Employee, Conference, FAQ, VAK, Thesis, Monograph, Plan, Income, RID
+
+
+class DuplicatesFilter(admin.SimpleListFilter):
+    title = "Дубликаты"
+    parameter_name = 'duplicates'
+    
+    def lookups(self, request, model_admin):
+        return [
+            ("dubl", ("Дубликаты")),
+            ("single", ("Без дубляжа")),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'dubl':
+            # Получение публикаций, которые имеют дубликаты
+            # Приводим название публикации к нижнему регистру и присваеваем этому полю 'lower_name'
+            # Группруем по новому полю 'lower_name'
+            # Считаем количество публикаций и записываем количество в поле 'count'
+            # Фильтруем записи, где 'count' больше 1 (есть дубликаты)
+            # Получаем словарь с ключами lower_name и count
+            duplicate_names = VAK.objects.annotate(lower_name=Lower('Name')).\
+                values('lower_name').\
+                annotate(count=Count('lower_name')).\
+                filter(Q(count__gt=1))
+    
+            # Формируем список из названием статей, которые хранятся в словаре duplicate_names с ключем 'lower_name'
+            list_duplicate = [dn['lower_name'] for dn in duplicate_names]
+
+            # Формируем queryset из статей из списка дубляжей, предварительно приводя все названия публикаций к нижнему регистру
+            return queryset.annotate(lower_name=Lower('Name')).filter(lower_name__in=list_duplicate)
+        else:
+            return queryset
 
 
 class InstituteAdmin(admin.ModelAdmin):
@@ -9,23 +44,23 @@ class InstituteAdmin(admin.ModelAdmin):
 
 
 class VAKAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'IdDeparture', 'Author', 'DateCreated')
+    list_display = ('Name', 'IdDeparture', 'Author', 'DateCreated', 'Accepted')
     list_display_links = ('Name', )
-    list_filter = ('IdDeparture','Author')
+    list_filter = ('IdDeparture','Author', DuplicatesFilter)
     search_fields = ('Name', 'IdDeparture__Name', 'Author__username')
     
 
 class ThesisAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'IdDeparture', 'Author', 'DateCreated')
+    list_display = ('Name', 'IdDeparture', 'Author', 'DateCreated', 'Accepted')
     list_display_links = ('Name', )
-    list_filter = ('IdDeparture','Author')
+    list_filter = ('IdDeparture','Author', DuplicatesFilter)
     search_fields = ('Name', 'IdDeparture__Name', 'Author__username')
     
     
 class MonographAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'IdDeparture', 'Author', 'DateCreated')
+    list_display = ('Name', 'IdDeparture', 'Author', 'DateCreated', 'Accepted')
     list_display_links = ('Name', )
-    list_filter = ('IdDeparture','Author')
+    list_filter = ('IdDeparture','Author', DuplicatesFilter)
     search_fields = ('Name', 'IdDeparture__Name', 'Author__username')
 
 
